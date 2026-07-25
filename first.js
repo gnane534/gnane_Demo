2bf803104dbeb01ed0c9c7a77fa0447f94bcdbac
Gnaneshwar Reddy <gnaneshwarreddy550@gmail.com>
2026-07-25 12:58:14 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,171 @@
+/*let a = 10;
+let b = 30;
+
+console.log("find the add ", a + b);
+console.log("find the sub", a - b);
+console.log("find the multiple", a * b);
+console.log("find the divisible", a / b);
+console.log("find the modules", a % b);
+console.log("find the power", a ** b);
+console.log("find the post incremnt", a++);
+console.log("find the increment", a);
+console.log("find the post decrement", a--);
+console.log("find the decrement", a);
+console.log("find the post increment", b++);
+console.log("find the increment", b);
+console.log("find the post incremnt", b--);
+console.log("find the decrement", b);
+console.log("find the pre increment", ++a);
+console.log("find the pre decrement", --b);
+
+
+//relational operators
+let a = 45;
+let b = 90;
+console.log("find the ais equal to b ", a == b);
+console.log("find the b is equal to a", b == a);
+console.log("a is greaer than b", a > b);
+console.log("b is greater than a", b > a);
+console.log("a is less than b ", a < b);
+console.log(" b is lessthan a", b < a);
+console.log("a is not equal to b", a != b);
+console.log("b is not equal to a", b != a);
+console.log("a is greater than or equal to b ", a >= b);
+console.log("b is greater than or equal to a", b >= a);
+console.log(" ais lessthan or eqaul to", a <= b);
+console.log("b is lessthan or equal to a", b <= a);
+console.log("a is double equal to b", a === b);
+console.log("b is double equal to a", b === a);
+console.log("a is not equal to b ", a !== b);
+console.log("b is not equal to a", b !== a);
+// logical operator
+let a = 45;
+let b = 23;
+console.log(a > b && b > a);
+console.log(a > b || b == a);
+console.log(!(a == b));
+
+let a = 34;
+let b = 54;
+let c = 67;
+if (a > b && b < a) {
+    console.log(" a is greater than b");
+} else if (b > a && c < b) {
+    console.log("b is greater than a and c ");
+} else {
+    console.log(" c is greater than all a and b",)
+}
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log("i", i);
+    } else {
+        console.log("odd numbers", i)
+    }
+}
+//whlie condition
+let i = 0;
+while (i <= 10) {
+    console.log("i", i);
+    i++;
+}
+//do while  loop
+let i = 2;
+do {
+    console.log("founding ...i", i);
+    i++;
+} while (i <= 10);
+
+
+// for in loop
+let name = "Gnaneshwar reddy";
+let length = 0;
+for (let val of name) {
+    console.log("val", val);
+    length++;
+}
+console.log("length is", length)
+//for in loops
+let student = {
+    name: "Gnaneshwar Reddy",
+    age: 21,
+    cgpa: 7.66,
+}
+for (let key in student) {
+    console.log("key", key, "values=", student[key])
+}
+console.log("length of name=", (student.name.length))
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log("i", i);
+
+    } else {
+        console.log("odd numbers", i);
+    }
+}
+let i = 0;
+while (i <= 100) {
+    if (i % 2 == 0) {
+        console.log("i", i);
+        i++;
+    } else {
+        console.log("odd", i);
+        i++;
+    }
+}
+let i = 0;
+do {
+    console.log("even or odd", i);
+    i++;
+    if (i % 2 == 0) {
+        console.log("i", i);
+        i++;
+    }
+} while (i <= 100);*/
+
+//strings
+/*let str = "gnaneshwar reddy"
+console.log(str);
+//length of string
+let str = "java scriptt"
+console.log("str=", str.length);
+
+//templates
+let name = `this my programing language of java script`;
+console.log(typeof name);
+
+let a = 10;
+let b = 34;
+let sum = `${a + b}`;
+console.log(sum);
+
+// methods in strings
+let str = prompt("enter the name:");
+let newstring = (str.toLowerCase());
+console.log("string", newstring);
+
+// arrays
+let companies = ["microsoft", "google", "capgemini", "uber", "ibm"];
+let remove = companies.splice(1, 1, "iqva");
+console.log(remove);*/
+
+
+/*let arr = ["maths", "physics", "chemistry", "java", "python", "c++"];
+for (let i = 0; i < arr.length; i++) {
+    console.log("arr", arr[i]);
+}
+console.log("length of arr", arr.length);*/
+
+/*let arr = ["mango", "apple", "orange", "pineapple"];
+for (let val of arr) {
+    console.log("val", arr);
+}*/
+/*let markes = [98, 87, 55, 68, 70, 90, 76, 66];
+let sum = 0;
+for (let val of markes) {
+    sum = sum + val / markes.length;
+    console.log("sum addind", sum);
+}
+console.log("average marks=", sum);*/
+
